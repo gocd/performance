7eb09b12e140330d56735fc49d8ada310a968809
rajiesh <rajiesh.narayanan@gmail.com>
2018-11-05 16:35:51 +0530

    Do not perform any server setup from agent node

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -387,7 +387,7 @@ module Configuration
     end
 
     def secure_url
-      env('PERF_SERVER_SSH_URL', 'https://localhost:8154')
+      env('PERF_SERVER_SSH_URL', "https://#{host}:8154")
     end
 
     def environment
