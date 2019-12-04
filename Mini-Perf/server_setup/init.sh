166a21bf38c91361490f67b3afd88df984bdb42b
rajiesh <rajiesh.narayanan@gmail.com>
2019-12-04 22:04:26 +0530

    Include Vector for monitoring

--- Mini-Perf/server_setup/init.sh
+++ Mini-Perf/server_setup/init.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-yum install -y which net-tools
+yum install -y which net-tools pcp
 
 echo "Setting up an empty Go config file, with site URLs set."
 tee /godata/config/cruise-config.xml >/dev/null <<EOF
 <?xml version="1.0" encoding="utf-8"?>
-<cruise xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="cruise-config.xsd" schemaVersion="134">
-  <server artifactsdir="artifacts" siteUrl="http://localhost:8153" secureSiteUrl="https://localhost:8154" agentAutoRegisterKey="perf-auto-register-key" commandRepositoryLocation="default" serverId="perf-server">
-  <security>
+<cruise xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="cruise-config.xsd" schemaVersion="132">
+  <server agentAutoRegisterKey="perf-auto-register-key" commandRepositoryLocation="default" serverId="perf-server">
+    <siteUrls>
+      <siteUrl>http://localhost:8153</siteUrl>
+      <secureSiteUrl>https://localhost:8154</secureSiteUrl>
+    </siteUrls>
+    <security>
       <authConfigs>
         <authConfig id="password_file" pluginId="cd.go.authentication.passwordfile">
           <property>
@@ -17,8 +21,13 @@ tee /godata/config/cruise-config.xml >/dev/null <<EOF
         </authConfig>
       </authConfigs>
     </security>
-   </server>
+    <backup emailOnSuccess="true" emailOnFailure="true" />
+    <artifacts>
+      <artifactsDir>artifacts</artifactsDir>
+    </artifacts>
+  </server>
 </cruise>
+
 EOF
 
 # Wait for Postgres to be ready and serving.
@@ -29,7 +38,6 @@ while [ "$count" -lt "30" -a "$(nc -zv db 5432 2>/dev/null; echo $?)" -ne "0" ];
   count=$((count + 1))
 done
 
-cp setup-pipelines.sh /docker-entrypoint.d/
 chown -R 1000:1000 /godata/*
 
 
