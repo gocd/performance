5fade81902085e32ba3a969cff46a4d191e5a046
rajiesh <rajiesh.narayanan@gmail.com>
2019-12-17 11:48:26 +0530

    Avoid cleaning up all containers and images
    Do clean up of only resources created by the test.
    Use unique port for perf server

--- micro_performance/lib/go_constants.rb
+++ micro_performance/lib/go_constants.rb
@@ -1,5 +1,5 @@
 module GoConstants
 
     AUTH_HEADER = "Basic #{Base64.encode64('admin:badger')}".freeze
-    BASE_URL = 'http://localhost:8153/go'.freeze
+    BASE_URL = 'http://localhost:8353/go'.freeze
 end
